bf2f5193ef698b3f8babf6085b5d031e788cf9a2
sadatakhtar <sadat0akhtar@gmail.com>
2021-02-23 12:29:10 +0000

    styled user input page a little and added results functionality

--- src/pages/userInputPage/UserInputPage.js
+++ src/pages/userInputPage/UserInputPage.js
@@ -10,14 +10,12 @@ function UserInputPage() {
     const [mensesStartDate, setMensesStartDate] = useState('');
     const [mensesEndDate, setMensesEndDate] = useState('');
     const [purityDuration, setPurityDuration] = useState(0);
-
     const [nextExpectedMensesStartDate, setNextExpectedMensesStartDate] = useState('');
     const [nextExpectedMensesEndDate, setNextExpectedMensesEndDate] = useState('');
 
     //Get number of days between 2 dates
     function getDiffInDays(date1, date2){
         return Math.round((date2 - date1) / (1000*60*60*24));
-
     }
 
     //Add days to existing date
@@ -27,10 +25,7 @@ function UserInputPage() {
         return result;
     }
 
-    function end(date, days){
-        let res = date + days;
-        return res;
-    }
+    //
 
     const handleStartDate = (e) => {
         setMensesStartDate(e.target.value);
@@ -52,10 +47,20 @@ function UserInputPage() {
         e.preventDefault();
        const startDate = new Date(mensesStartDate);
        const endDate = new Date(mensesEndDate);
+
        let res = getDiffInDays(new Date(mensesStartDate), new Date(mensesEndDate));
        setMensesDuration(res);
+
        let daysResult = addDays(mensesEndDate, purityDuration);
        setNextExpectedMensesStartDate(daysResult);
+
+       //calculate next menses end date
+       let nextExMensesEndDate = addDays(nextExpectedMensesStartDate, mensesDuration);
+      setNextExpectedMensesEndDate(nextExMensesEndDate);
+      console.log(nextExMensesEndDate);
+   
+
+
     }
 
     //Reset selected dates and figures
@@ -65,6 +70,7 @@ function UserInputPage() {
         setMensesDuration(0);
         setPurityDuration(0);
         setNextExpectedMensesStartDate('');
+        setNextExpectedMensesEndDate('');
     }
     return (
         <div className="user_container">
@@ -73,21 +79,22 @@ function UserInputPage() {
 
             </div>
             <div className="user_body">
+              
            
 
                 <div className="user_form">
-                    <h1>User Input Page</h1>
+                    <h1>User Input</h1>
                     <form onSubmit={handleSubmit}>
-                        <label>Menses Start Date:
+                        <label>Menses Start Date: <br/>
                         <input type="date" onChange={handleStartDate} required/>
                         </label>
 
-                        <label>Menses End Date: 
+                        <label>Menses End Date: <br/>
                         <input type="date"  onChange={handleEndDate} required/>
                         </label>
                         
-                        <label>Purity Duration: 
-                        <input type="number" onChange={handlePurityDuration} required/>
+                        <label>Purity Duration: <br/>
+                        <input id="purity" type="number" onChange={handlePurityDuration} required/>
                         </label>
                         
                         <button >Submit</button>
@@ -95,9 +102,10 @@ function UserInputPage() {
                     </form>
                    
                     <div className="user_results">
+                        <h1>Results</h1>
                     
-                        <p>{mensesStartDate}</p>
-                        <p>{mensesEndDate}</p>
+                        <p>{`Menses began on ${mensesStartDate}`}</p>
+                        <p>{`Menses ended on ${mensesEndDate}`}</p>
                         <p>{`Duration of your menses is ${mensesDuration} days`}</p>
                         <p>{`Next expected menses start date is ${nextExpectedMensesStartDate}`}</p>
                         <p>{`Next expected menses end date is ${nextExpectedMensesEndDate}`}</p>
